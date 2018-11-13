91e4ce1abb26065796b023c9b22162303c565bf7
xueyanmin <2263056159@qq.com>
2018-11-13 20:09:02 +0800

    MS.LI

---
+++ src/main/java/com/entity/TreGit.java
@@ -0,0 +1,8 @@
+package com.entity;
+
+public class TreGit {
+    public static void main(String[] args) {
+        System.out.println("I am Ms.li");
+        System.out.println("I am Ms.li***********************");
+    }
+}
