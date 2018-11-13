a251de87eef5575bb3bdeb390ea24e758a558bc3
xueyanmin <2263056159@qq.com>
2018-11-13 19:51:31 +0800

    init

---
+++ src/main/java/com/entity/add.java
@@ -0,0 +1,30 @@
+package com.entity;
+
+public class add {
+    private int id;
+    private String name;
+
+    public add() {
+    }
+
+    public add(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
